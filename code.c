0d41ecbba6754696d50234280241cfd1a1ec1d58
Ratheshprabakar <ratheshprabakar@gmail.com>
2020-07-10 20:06:24 +0530

    Added Card Creation module

--- code.c
+++ code.c
@@ -1,32 +1,56 @@
 //Virtual Bank Gift Card Management System
 #include<stdio.h>
-void create();
-//void login();
+void acc_create();
+void card_create();
 int choice,i=1,j;
 struct acccount_details
 {
-char name[100];
-int age;
-int balance;
+	char name[100];
+	int age;
+	int balance;
 }acc[100];
+struct card_details
+{
+	int id,pin;
+	int amount;
+}card[100];
 char temp_name[100];
 int temp_age;
 int temp_balance;
+int card_no=1;
 int main()
 {
 
 	while(1)
 	{
 		printf("\n\nWelcome to Bank Gift Card System\n\n");
-		printf("1) Account Creation\n2) Already have the account ? (Login)\n3) Exit\nPlease Enter Your Choice\t");
+		printf("1) Account Creation\n2)Gift Card\n3)Exit\nPlease Enter Your Choice\t");
 		scanf("%d",&choice);
 		switch(choice)
 		{
 			case 1:
-				create();
+				acc_create();
 				break;
 			case 2:
-				//login();
+				printf("\n1)Create gift card\n2)Top-Up gift card\n3)Close gift card\n4)Back\nPlease Enter your choice\t");
+				scanf("%d",&choice);
+				switch(choice)
+				{
+					case 1:
+						card_create();
+						break;
+					/*case 2:
+						card_topup();
+						break;
+					case 3:
+						card_close();
+						break;*/
+					case 4:
+						main();
+						break;
+					default:
+						printf("\nPlease Enter valid choice\n");		
+				}
 				break;
 			case 3:
 				exit(1);
@@ -36,7 +60,7 @@ int main()
 	}
 	
 }
-void create()
+void acc_create()
 {
 	printf("\n\nWelcome to account creation\n\n");
 	printf("Enter Your Name\t");
@@ -65,4 +89,37 @@ void create()
 		i++;	
 	}
 	
+}
+void card_create()
+{
+	int id;
+	printf("Enter Your CustID\t");
+	scanf("%d",&id);
+	if(id<i && id!=0)
+	{
+		printf("\n\nYour Acount contains %d balance",acc[id].balance);
+		printf("\n\nEnter the Amount which you want to buy as gift card\t");
+		scanf("%d",&temp_balance);
+		if(temp_balance<=acc[id].balance)
+		{
+			card[card_no].id=id;
+			printf("Enter the PIN\t");
+			scanf("%d",&card[card_no].pin);
+			card[card_no].amount=temp_balance;
+			acc[id].balance-=temp_balance;
+			temp_balance=0;
+			printf("Your Gift Card is successfully purchased\nCardNo\tAmount\n%d\t%d",card_no,card[card_no].amount);
+			card_no++;
+		}
+		else
+		{
+			printf("\n\nSorry, Your account doesn't contain sufficient balance to buy the card\n\n");
+			main();
+		}
+		
+	}
+	else
+	{
+		printf("\n\nSorry Your CustID is not found, Please create an account!!\n\n");	
+	}
 }
